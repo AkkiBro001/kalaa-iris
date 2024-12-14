f665d31d0297100e9b2fad0c98c9926d53b44a5c
Akshay Tambe <akshay.design4art@gmail.com>
2024-12-14 14:50:23 +0530

    Form validation done

--- src/components/franchise/franchise-form.tsx
+++ src/components/franchise/franchise-form.tsx
@@ -9,20 +9,23 @@ import {
   SelectContent,
   SelectItem,
 } from "@/components/ui/select";
-import {countries} from "../../lib/countries";
-import { Textarea } from "@/components/ui/textarea"
+import { countries } from "../../lib/countries";
+import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { useForm, FormProvider } from "react-hook-form"
+import { useForm, FormProvider, Controller } from "react-hook-form";
+import { cn } from "@/lib/utils";
+
 
 export default function FranchiseForm() {
-
   const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
   const MOBILE_REGEX = /^\+?[1-9][0-9]{9,11}$/g;
 
-  const methods = useForm()
-  const { register } = methods;
+  const methods = useForm();
+  const { register, control, setValue, formState: {errors} } = methods;
 
-  const onSubmit = (data:unknown) => console.log(data)
+
+
+  const onSubmit = (data: unknown) => console.log(data);
 
   return (
     <section className="px-6 py-20 flex flex-col md:flex-row gap-8 items-center ">
@@ -42,60 +45,138 @@ export default function FranchiseForm() {
         </header>
 
         <section>
-        <FormProvider {...methods}>
+          <FormProvider {...methods}>
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={methods.handleSubmit(onSubmit)}
+            >
+              <div className="flex gap-4">
+                <InputGroup short_code="fname">
+                  <Input
+                    placeholder="First Name"
+                    {...register("fname", {
+                      required: {
+                        value: true,
+                        message: "First Name is required",
+                      },
+                    })}
+                    className={cn(errors.fname && "focus-visible:ring-red-600 ring-2 ring-offset-2 ring-red-600")}
+                  />
+                </InputGroup>
+                <InputGroup short_code="lname">
+                  <Input
+                    placeholder="Last Name"
+                    {...register("lname", {
+                      required: {
+                        value: true,
+                        message: "Last Name is required",
+                      },
+                    })}
+                    className={cn(errors.lname && "focus-visible:ring-red-600 ring-2 ring-offset-2 ring-red-600")}
+                  />
+                </InputGroup>
+              </div>
 
-          <form className="flex flex-col gap-4" onSubmit={methods.handleSubmit(onSubmit)}>
-            <div className="flex gap-4">
-              <InputGroup short_code="fname">
-                <Input placeholder="First Name" {...register('fname', {required: {value: true, message: "First Name is required"}})}/>
-              </InputGroup>
-              <InputGroup short_code="lname">
-                <Input placeholder="Last Name"  {...register('lname', {required: {value: true, message: "Last Name is required"}})}/>
-              </InputGroup>
-            </div>
+              <div className="flex gap-4">
+                <InputGroup short_code="email">
+                  <Input
+                    placeholder="Email Address"
+                    {...register("email", {
+                      required: {
+                        value: true,
+                        message: "Last Name is required",
+                      },
+                      pattern: {
+                        value: EMAIL_REGEX,
+                        message: "Email Address is invaild",
+                      },
+                    })}
+                    className={cn(errors.email && "focus-visible:ring-red-600 ring-2 ring-offset-2 ring-red-600")}
+                  />
+                </InputGroup>
+                <InputGroup short_code="mobile">
+                  <Input
+                    placeholder="Mobile Number"
+                    {...register("mobile", {
+                      required: {
+                        value: true,
+                        message: "Mobile Number is required",
+                      },
+                      pattern: {
+                        value: MOBILE_REGEX,
+                        message: "Mobile No. is invalid",
+                      },
+                    })}
+                    type="text"
+                    className={cn(errors.mobile && "focus-visible:ring-red-600 ring-2 ring-offset-2 ring-red-600")}
+                  />
+                </InputGroup>
+              </div>
 
-            <div className="flex gap-4">
-              <InputGroup short_code="email">
-                <Input placeholder="Email Address" {...register('email', {required: {value: true, message: "Last Name is required"}, pattern: {value: EMAIL_REGEX, message: "Email Address is invaild"}})}/>
-              </InputGroup>
-              <InputGroup short_code="mobile">
-                <Input placeholder="Mobile Number" {...register('mobile', {required: {value: true, message: "Mobile Number is required"}, pattern: {value: MOBILE_REGEX, message: "Mobile No. is invalid"}})} type="text"/>
-              </InputGroup>
-            </div>
+              <div className="flex gap-4">
+                <InputGroup short_code="country">
+                  <Controller
+                    control={control}
+                    name="country"
+                    defaultValue={"India"}
+                    render={({ field}) => (
+                      <Select defaultValue="India" {...field} onValueChange={(value) => setValue("country", value)}>
+                        <SelectTrigger className="text-black">
+                          <SelectValue placeholder="Select Country" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {countries.map((country) => (
+                            <SelectItem
+                              key={country.name}
+                              value={country.value}
+                            >
+                              {country.name}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    )}
+                  />
+                </InputGroup>
+              </div>
 
-            <div className="flex gap-4">
-              <InputGroup short_code="country">
-                <Select defaultValue="India">
-                  <SelectTrigger className="text-black">
-                    <SelectValue placeholder="Select Country"/>
-                  </SelectTrigger>
-                  <SelectContent>
-                    {
-                      countries.map(country => <SelectItem key={country.name} value={country.value}>{country.name}</SelectItem>)
-                    }
-                    
-                    
-                  </SelectContent>
-                </Select>
-              </InputGroup>
-            </div>
+              <div className="flex gap-4">
+                <InputGroup short_code="state">
+                  <Input
+                    placeholder="State"
+                    {...register("state", {
+                      required: { value: true, message: "State is required" },
+                    })}
+                    className={cn(errors.state && "focus-visible:ring-red-600 ring-2 ring-offset-2 ring-red-600")}
+                  />
+                </InputGroup>
+                <InputGroup short_code="city">
+                  <Input
+                    placeholder="City"
+                    {...register("city", {
+                      required: { value: true, message: "City is required" },
+                    })}
+                    className={cn(errors.city && "focus-visible:ring-red-600 ring-2 ring-offset-2 ring-red-600")}
+                  />
+                </InputGroup>
+              </div>
 
-            <div className="flex gap-4">
-              <InputGroup short_code="state">
-                <Input placeholder="State" {...register('state', {required: {value: true, message: "State is required"}})}/>
-              </InputGroup>
-              <InputGroup short_code="city">
-                <Input placeholder="City" {...register('city', {required: {value: true, message: "City is required"}})}/>
-              </InputGroup>
-            </div>
+              <div>
+                <Textarea
+                  placeholder="How did you hear about us?"
+                  className="text-black"
+                  {...register("source")}
+                />
+              </div>
 
-            <div>
-              <Textarea placeholder="How did you hear about us?" className="text-black" {...register('source')}/>
-            </div>
-
-            <Button className="xs:w-full md:w-[200px] bg-primaryColor" type="submit">Submit</Button>
-          </form>
-        </FormProvider>
+              <Button
+                className="xs:w-full md:w-[200px] bg-primaryColor"
+                type="submit"
+              >
+                Submit
+              </Button>
+            </form>
+          </FormProvider>
         </section>
       </div>
     </section>
